6d387ba628033940a3cde2770c0452c798bcf53e
関根豪 <goukai0512@icloud.com>
2025-06-09 16:09:55 +0900

    4

--- Assets/Script/StatusController.cs
+++ Assets/Script/StatusController.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatusController : MonoBehaviour
 {
-    public int stress = 0;
+    public float stress = 0;
     public int intelli = 0;
     public int skill = 0;
     public int commu = 0;
+
+    [SerializeField] private Slider stressSlider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //stressSlider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        stressSlider.value = stress;
     }
 }
--- Assets/Script/TextController.cs
+++ Assets/Script/TextController.cs
@@ -6,20 +6,16 @@ using UnityEngine.UI;
 
 public class TextController : MonoBehaviour
 {
-    public TextMeshPro countDown;
+    public TextMeshProUGUI countDown;
     private float time = 0;
 
     public bool isCountFinish = false;
 
-    private Renderer r;
-
-    [SerializeField] private GameObject Camera;
     [SerializeField] private PlayerController playerController;
 
     // Start is called before the first frame update
     void Start()
     {
-        r = countDown.GetComponent<Renderer>();
         countDown.text = "3";
     }
 
@@ -36,17 +32,14 @@ public class TextController : MonoBehaviour
             countDown.text = "GO";
             if(3 - time < -0.5)
             {
-                r.enabled = false;
+                countDown.enabled = false;
             }
         }
         if(playerController.isGoal == true)
         {
             countDown.text = "Goal";
-            r.enabled = true;
+            countDown.enabled = true;
         }
         time += Time.deltaTime;
-        
-
-        //transform.position = new Vector3(transform.position.x, transform.position.y, Camera.transform.position.z + 2.7f);
     }
 }
